7c635eb3db67213bf383e5a43924cdd028f4f1ab
Robert Lee-Cann <leeky@leeky.org.uk>
2009-12-18 14:00:35 +0000

    Make demo page actually do something useful.

--- master.js
+++ master.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
 	var fonts_used = new Array();
 	
-	var fontstacks = $('*').each(function(i) { 
+	var fontstacks = $('body *').each(function(i) { 
 
 		fontstack = $(this).css('font-family').split(',');
 		fontstack = jQuery.map(fontstack, function(current) { 
 			return current.replace(new RegExp("\w+", "g"), "");
 		});   
-		
-		console.warn(fontstack);
-		
+			
 		jQuery.each(fontstack, function(i,v) {
 	
 		   if (jQuery.inArray(v, fonts_used) == -1) {
@@ -19,6 +17,28 @@ $(document).ready(function(){
 		}); 
 		
 	});
-
-	console.log(fonts_used); 
+	
+	// GUI  
+	gui = $('body').append('<div id="fontstacker_gui" style="position: absolute; top: 0; left: 0; background-color: #ccc; display: block; padding: 10px; width: 95%;">Font Stack Tester: </div>');
+	
+	jQuery.each(fonts_used, function(i,v) {
+	   $('#fontstacker_gui').append('<button id="fontstacker_toggle_' + v + '">Remove ' + v + '</button>'); 
+	   
+	   $('#fontstacker_toggle_' + v).click(function() {
+			fontstacker_remove_font(v);
+	   });
+	});                           
+    
 });
+
+function fontstacker_remove_font(font) {
+	
+	var fontstacks = $('body *').each(function(i,v) { 
+
+		fontstack = $(v).css('font-family').replace(font, 'zzz'); 
+		$(v).css('font-family', fontstack);
+
+	});
+	 
+	$('#fontstacker_toggle_' + font).attr('disabled','disabled');
+}
